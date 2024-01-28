b467add81824dc6d224a9c53d41619be5caa71a6
jonah <122654199+jonuuh@users.noreply.github.com>
2024-01-28 01:25:01 -0800

    Update ExampleMod.java

--- src/main/java/net/example/examplemod/ExampleMod.java
+++ src/main/java/net/example/examplemod/ExampleMod.java
@@ -1,19 +1,18 @@
-package net.example.examplemod;
+package net.jonuuh.examplemod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(
+        modid = "examplemod",
+        version = "1.2.0",
+        acceptedMinecraftVersions = "[1.8.9]"
+)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        System.out.println("DIRT BLOCK: " + Blocks.dirt.getUnlocalizedName());
     }
 }
