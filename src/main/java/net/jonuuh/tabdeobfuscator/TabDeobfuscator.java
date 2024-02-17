123155c5f35c7ff4de97197741dbd6ae1721f608
jonuuh <jhammond.123x@gmail.com>
2024-02-17 03:54:03 -0800

    v1.1.2 - more cleanup

--- src/main/java/net/jonuuh/tabdeobfuscator/TabDeobfuscator.java
+++ src/main/java/net/jonuuh/tabdeobfuscator/TabDeobfuscator.java
@@ -1,44 +1,16 @@
 package net.jonuuh.tabdeobfuscator;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.scoreboard.Scoreboard;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(
-        modid = "tabdeobfuscator",
-        version = "1.1.1",
-        acceptedMinecraftVersions = "[1.8.9]"
-)
+@Mod(modid = "tabdeobfuscator", version = "1.1.2", acceptedMinecraftVersions = "[1.8.9]")
 public class TabDeobfuscator
 {
-    private static Minecraft mc;
-    private static GuiPlayerTabOverride tabGui;
-
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        mc = Minecraft.getMinecraft();
-        tabGui = new GuiPlayerTabOverride(mc, mc.ingameGUI);
         MinecraftForge.EVENT_BUS.register(new TabEvent());
-    }
-
-    private static class TabEvent
-    {
-        @SubscribeEvent
-        public void onPreRenderTabGui(RenderGameOverlayEvent.Pre event)
-        {
-            if (event.type == RenderGameOverlayEvent.ElementType.PLAYER_LIST && Util.isAnyNameObfuscated(mc))
-            {
-                event.setCanceled(true);
-                Scoreboard sb = mc.theWorld.getScoreboard();
-                tabGui.renderPlayerlist(new ScaledResolution(mc).getScaledWidth(), sb, sb.getObjectiveInDisplaySlot(0));
-            }
-        }
     }
 }
